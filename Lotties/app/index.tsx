27b259439cd5ce14395f6b966c4e525861da6e8d
Manol Draganov <678785@student.inholland.nl>
2025-12-04 08:59:46 +0100

    adding color pallete

--- Lotties/app/index.tsx
+++ Lotties/app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { palette } from "./constants/colors";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello world</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: palette.background.main,
+  },
+  text: {
+    color: palette.text.main,
+    fontSize: 20,
+    fontWeight: "600",
+  },
+});
